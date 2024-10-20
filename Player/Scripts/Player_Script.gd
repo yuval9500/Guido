d01f3d3b3da573e67fdfcde6383d151584d5837f
Yuvalr <yuval9500@gmail.com>
2024-10-20 19:03:15 +0300

    Companion following player + test level

--- Player/Scripts/Player_Script.gd
+++ Player/Scripts/Player_Script.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
-@export var speed = 400
+class_name Player_Script
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+@export var speed = 400:
+	get:
+		return speed
 
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
