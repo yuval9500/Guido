f93749725c1b7180c2240554ce0e258488487a58
Yuvalr <yuval9500@gmail.com>
2024-10-20 22:52:46 +0300

    dialogue ui fix + start of combat player

--- NPC/Scripts/npc.gd
+++ NPC/Scripts/npc.gd
@@ -1,21 +1,25 @@
 extends Node2D
 
 var canInteract = false
+var dialogueUI: DialogueUI
+
+func _ready() -> void:
+	dialogueUI = get_tree().get_root().find_child("DialogueUI",true,false)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("body entered")
-	canInteract = true
-	pass
+	if body.name == "Player":
+		print("body entered")
+		canInteract = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	print("body exited")
-	canInteract = false
-	pass 
+	if body.name == "Player":
+		print("body exited")
+		canInteract = false 
 
 func _input(event: InputEvent) -> void:
 	dialogue(event)
 	
 func dialogue(event: InputEvent) -> void:
 	if canInteract and event.is_action_pressed("talk"):
-		$"../Player/DialogueUI".startDialogue("first","second")
+		dialogueUI.startDialogue("first","second")
 		print("pressed e")
