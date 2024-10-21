19198e0b13a259d612c1eea81a553921f84d55b4
Eido Peretz <54716567+eido321@users.noreply.github.com>
2024-10-21 16:56:01 +0300

    dialogueUi_V2 and StoryManager_V1

--- DialogueUI/scripts/dialogue_ui.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-class_name DialogueUI
-
-func startDialogue(nameText ,dialogueText) -> void:
-	self.visible = true
-	$nameCon/Label.text = nameText
-	$dialogueCon/Label.text = dialogueText
-	
