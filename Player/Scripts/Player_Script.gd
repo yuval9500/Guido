45f7b7d15f84782d4f26377e77b60853f23f251c
Eido Peretz <54716567+eido321@users.noreply.github.com>
2024-10-29 00:16:26 +0200

    PauseMenu V2

--- Player/Scripts/Player_Script.gd
+++ Player/Scripts/Player_Script.gd
@@ -26,12 +26,20 @@ func _physics_process(_delta):
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		if pauseMenu.visible:
+			var menuGroup = pauseMenu.find_child("MenuGroup")
+			var optionGroup = pauseMenu.find_child("OptionsGroup")
+			if optionGroup.visible:
+				optionGroup.visible = false
+				menuGroup.visible = true
+			GameManager.resumeGame()
 			pauseMenu.visible = false
 		else:
+			GameManager.pauseGame()
 			pauseMenu.visible = true
 
 func get_input():
 	if GameManager.isPaused:
+		velocity = Vector2.ZERO
 		return
 		
 	var input_direction = Input.get_vector("left", "right", "up", "down")
